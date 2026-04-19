273237147fc1c5514f81fa54e9136f486a6413b1
tolga-a781 <tolga.beyti2020@gmail.com>
2026-04-19 23:13:51 +0300

    Add some more commands

--- src/bg/tu_varna/turing_machine/models/Transition.java
+++ src/bg/tu_varna/turing_machine/models/Transition.java
@@ -1,4 +1,23 @@
 package bg.tu_varna.turing_machine.models;
 
 public class Transition {
+    private String fromState;
+    private String read;
+    private String toState;
+    private String write;
+    private Direction direction;
+
+    public Transition(String fromState, String read, String toState, String write, Direction direction) {
+        this.fromState = fromState;
+        this.read = read;
+        this.toState = toState;
+        this.write = write;
+        this.direction = direction;
+    }
+
+    public String getFromState() { return fromState; }
+    public String getRead() { return read; }
+    public String getToState() { return toState; }
+    public String getWrite() { return write; }
+    public Direction getDirection() { return direction; }
 }
